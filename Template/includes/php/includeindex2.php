d1fa76049f45381c55be5bef246a418f9e1d08cd
Dylan Lang <dylanlang999@gmail.com>
2015-12-29 23:54:31 -0800

    Update and rename includetopabout.php to includeindex2.php

--- Template/includes/php/includeindex2.php
+++ Template/includes/php/includeindex2.php
@@ -5,4 +5,4 @@
     <title>Bootstrap 3 Responsive Design Tutorial | RevillWeb.com</title>
     <!-- Include bootstrap CSS -->
     <link href="includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="includes/about.css" rel="stylesheet">
+    <link href="includes/index2.css" rel="stylesheet">
