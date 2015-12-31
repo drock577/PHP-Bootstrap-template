59871960043c828b4840ddcbd36c21f3036ec23d
Dylan Lang <dylanlang999@gmail.com>
2015-12-30 18:55:19 -0800

    Update includeindex.php

--- Template/includes/php/includeindex.php
+++ Template/includes/php/includeindex.php
@@ -1,8 +1,12 @@
+    <!--- Meta date for the site -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--- Your sites Favicon -->
     <link rel="icon" type="image/png" href="img/favicon.png">
-    <!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+    <!-- Set the viewport to display on mobile devices -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 3 Responsive Design Tutorial | RevillWeb.com</title>
+    <!--- Webiste title to display in tabs menu -->
+    <title>PHP-Bootstrap-template</title>
     <!-- Include bootstrap CSS -->
     <link href="includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Include Custom CSS -->
     <link href="includes/index.css" rel="stylesheet">
