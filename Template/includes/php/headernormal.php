995c01fe754ed11fe039cae2f4d014d804a8e692
Dylan Lang <dylanlang999@gmail.com>
2016-02-10 20:05:47 -0800

    Create headernormal.php

---
+++ Template/includes/php/headernormal.php
@@ -0,0 +1,21 @@
+<header class="top" role="header">
+
+        <!--The titles of this-->
+
+        <div class="lobster r1">
+          <h1>Announcement</h1>
+        </div>
+
+        <!--Navbar-->
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="navbar-nav nav">
+                    <li class="nspace lobster"><a href="index.php"><span class="glyphicon glyphicon-home"></span> index</a></li>
+                    <li class="nspace lobster"><a href="index2.php"><span class="glyphicon glyphicon-user"></span> index2</a></li>
+                    <li class="nspace lobster"><a href="normal.php"><span class="glyphicon glyphicon-facetime-video"></span> Normal</a></li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+      </header>
