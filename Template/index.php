9530f80e01fcd3047d1c0c667739889282d5bf63
Dylan Lang <dylanlang999@gmail.com>
2016-02-10 20:03:52 -0800

    Changed the Click Here to Normal.php

--- Template/index.php
+++ Template/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <title>About the Project</title>
-        <?php include("includes/php/includeindex.php"); ?>
+        <title>About Me</title>
+        <?php include("includes/php/includeindex2.php"); ?>
 </head>
 <body>
 
@@ -13,19 +13,18 @@
     <!-- Site banner -->
     <div class="banner">
         <div class="container textcenter">
-            <strong><h1>The top banner.</h1></strong>
-            <strong><p><a href="/index2.php">Click Here</a> to go to the next page.</p></strong>
+            <strong><h1>The Top Banner</h1></strong>
+            <strong><p><a href="/normal.php">Click Here</a> to go to the next page.</p></strong>
         </div>
-    </div>    
-
+    </div>
  <div class="middle">
-    <div class="container textcenter">
+        <div class="container textcenter">
         <h1>The Middle contents section.</h1>
     </div>
 </div>
 
     <!-- Middle content section -->
-    
+
 
     <!-- Site footer -->
     <?php include("includes/php/footer.php"); ?>
