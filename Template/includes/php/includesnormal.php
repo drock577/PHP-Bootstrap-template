ef6de5e40b0cb1a1223a0a909b9e8dbb7600a6e7
Dylan Lang <dylanlang999@gmail.com>
2016-02-10 19:38:09 -0800

    Create includesnormal.php

---
+++ Template/includes/php/includesnormal.php
@@ -0,0 +1,8 @@
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="icon" type="image/png" href="img/favico.png">
+    <!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 3 Responsive Design Tutorial | RevillWeb.com</title>
+    <!-- Include bootstrap CSS -->
+    <link href="includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="includes/main.css" rel="stylesheet">
