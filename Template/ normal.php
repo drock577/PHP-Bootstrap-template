36917cdb9eb3f2299d74b5229bd0fc4e17c6fd6b
Dylan Lang <dylanlang999@gmail.com>
2016-02-10 20:04:17 -0800

    Create  normal.php

---
+++ Template/ normal.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>About Me</title>
+        <?php include("includes/php/includesnormal.php"); ?>
+</head>
+<body>
+
+    <!-- Site header and navigation -->
+    <?php include("includes/php/headernormal.php"); ?>
+
+
+    <!-- Site banner -->
+    <div class="green">
+        <div class="container textcenter">
+            <strong><h1>The Top Banner</h1></strong>
+            <strong><p><a href="/index.php">Click Here</a> to go to the next page.</p></strong>
+        </div>
+    </div>
+ <div class="middle wtext">
+        <div class="container textcenter">
+        <h1>The Middle contents section.</h1>
+    </div>
+</div>
+
+    <!-- Middle content section -->
+
+
+    <!-- Site footer -->
+    <?php include("includes/php/footer.php"); ?>
+    <?php include("includes/php/includebottom.php"); ?>
+</body>
+</html>
